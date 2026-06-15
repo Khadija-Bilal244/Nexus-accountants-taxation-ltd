33ad7f215bb27d408600fdc25f86edd7ad0712a2
Khadija Bilal <khadija.bilal9167@gmail.com>
2026-06-15 17:48:18 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,39 +17,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-/*import './App.css';
-
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} BrandName. All rights reserved.</p>
-    </footer>
-  );
-}
-
-function Navbar() {
-  return (
-    <nav>
-      <ul className="navbar">
-        <li><a href="#Home">Home</a></li>
-        <li><a href="#Services">Services</a></li>
-        <li><a href="#Features">Features</a></li>
-        <li><a href="#Careers">Careers</a></li>
-        <li><a href="#Contact">Contact</a></li>
-      </ul>
-    </nav>
-  );
-}
-
-export default function App() {
-  return (
-    <div className="app-container">
-      <Navbar />
-      <Footer />
-    </div>
-  );
-}*/
